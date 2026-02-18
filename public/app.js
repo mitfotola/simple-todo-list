5f56a8009c8a2164e07199ee3796e3847f9be6a1
mitfotola <akanat.apiluckpanich@gmail.com>
2026-02-18 17:39:05 +0700

    second

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ const todoList = document.getElementById('todoList');
 const totalCount = document.getElementById('totalCount');
 const completedCount = document.getElementById('completedCount');
 
-// State
+// State eiei
 let todos = [];
 
 // Fetch all todos
